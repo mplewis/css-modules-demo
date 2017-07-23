c406ed6733e8c3fd9bca44e452ed6a1c0d07de6a
Matt Lewis <matt@mplewis.com>
2017-07-23 12:08:40 -0500

    git: initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# CSS Modules Demo
+
+Shows how to style a single JSX component with multiple conditional or unconditional classes using CSS modules.
+
+# Run
+
+```sh
+yarn install
+yarn dev
+```
+
+# License
+
+MIT
---
+++ index.jsx
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import style from './index.scss'
+
+const Component = {
+  name: 'Component',
+  data: () => ({
+    red: false,
+    strong: false,
+  }),
+  render() {
+    return (
+      <div>
+        <h1>CSS modules demo</h1>
+        <hr />
+        <p class={ [style.sample, style.italic] }>
+          This content is styled unconditionally with <code>sample</code> and <code>italic</code>
+        </p>
+        <hr />
+        <p class={
+          {
+            [style.sample]: true,
+            [style.red]: this.red,
+            [style.strong]: this.strong
+          }
+        }>
+          This content is styled conditionally, but always has the <code>sample</code> class enabled
+        </p>
+        <p>
+          <input type="checkbox" onChange={() => this.red = !this.red} />
+          class: <code>red</code>
+        </p>
+        <p>
+          <input type="checkbox" onChange={() => this.strong = !this.strong} />
+          class: <code>strong</code>
+        </p>
+      </div>
+    )
+  }
+}
+
+const app = new Vue(Component)
+app.$mount('#app');
